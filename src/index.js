cb81515cd9a224768b7e0ba16721d0cf1a805279
Nub <wilhelmjanmapogi@gmail.com>
2021-10-02 20:45:07 +0800

    Add event listeners

--- src/index.js
+++ src/index.js
@@ -1,13 +1,27 @@
 import "./style.css";
-// import getHome from "./home"
+import getHome from "./home";
+import getMenu from "./menu";
 
-// function loadContent(func) {
-//     const mainContent = document.querySelector('#content');
-//     const contentDiv = func()
-//         // New function that removes all nodes if no params.
-//         // Accepts a lot of nodes as param. Check mdn
-//     mainContent.replaceChildren(contentDiv);
-// }
+function loadContent(func) {
+    // e.prevetDefault();
+    const mainContent = document.querySelector('#content');
+    const contentDiv = func()
+        // New function that removes all nodes if no params.
+        // Accepts a lot of nodes as param. Check mdn
+    mainContent.replaceChildren(contentDiv);
+
+}
+const home = document.querySelector('#home');
+const menu = document.querySelector('#menu');
+
+home.addEventListener('click', (e) => {
+    e.preventDefault();
+    loadContent(getHome);
+})
+menu.addEventListener('click', (e) => {
+    e.preventDefault();
+    loadContent(getMenu);
+});
 
 
-// window.onload = loadContent(getHome)
+window.onload = loadContent(getHome);
