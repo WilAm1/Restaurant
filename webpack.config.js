decd07c32d27e29d4297a13d5296a44e2787f949
Nub <wilhelmjanmapogi@gmail.com>
2021-10-02 10:43:55 +0800

    Fix deleting of index html and webpack server

--- webpack.config.js
+++ webpack.config.js
@@ -4,11 +4,13 @@ module.exports = {
     mode: 'development',
     entry: './src/index.js',
     devtool: 'inline-source-map',
-
+    devServer: {
+        static: './dist',
+    },
     output: {
         filename: "main.js",
         path: path.resolve(__dirname, 'dist'),
-        clean: true,
+        // clean: true,
     }
 
 
