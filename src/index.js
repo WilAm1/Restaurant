38cca1f41fddbcbcaa0255589403cd152a2ea932
Nub <wilhelmjanmapogi@gmail.com>
2021-10-02 14:38:03 +0800

    Add preliminary style

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 import "./style.css";
+// add this to your seperate module after
+// import Icon from "./images/wc-icon.webp";
+
 
 console.log("helllo world")
--- webpack.config.js
+++ webpack.config.js
@@ -10,9 +10,14 @@ module.exports = {
 
     module: {
         rules: [{
-            test: /\.css$/i,
-            use: ['style-loader', 'css-loader'],
-        }, ],
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif|webp)$/i,
+                type: 'asset/resource',
+            },
+        ],
     },
 
     output: {
